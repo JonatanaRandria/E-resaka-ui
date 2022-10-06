3cb5ab315deae9c46b64c9480834bd36adc9634d
jonas4884 <hei.jonatana@gmail.com>
2022-10-06 06:26:14 +0200

    feat: add circle button

--- lib/components/circle_button.dart
+++ lib/components/circle_button.dart
@@ -1,19 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class CircleButton extends StatelessWidget {
-  const CircleButton({super.key});
+  final IconData icon;
+  final double iconSize;
+  final Function onPressed;
+
+  const CircleButton(
+      {Key? key,
+      required this.icon,
+      required this.iconSize,
+      required this.onPressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.all(6.0),
-        decoration:
-            BoxDecoration(color: Colors.grey[200], shape: BoxShape.circle),
-        child: IconButton(
-            icon: Icon(Icons.message),
-            iconSize: 30.0,
-            color: Colors.black,
-            onPressed: (() {})));
+      margin: EdgeInsets.all(6.0),
+      decoration:
+          BoxDecoration(color: Colors.grey[200], shape: BoxShape.circle),
+      child: IconButton(
+        icon: Icon(icon),
+        iconSize: iconSize,
+        color: Colors.black,
+        onPressed: (() => onPressed),
+      ),
+    );
   }
 }
